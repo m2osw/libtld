55d36b7fdfc2c2d1bcf9a6755635f97619f99d70
AlexisWilke <alexis@m2osw.com>
2022-07-09 07:43:06 -0700

    Removed auto-init comments.

--- libtld/tld_emails.cpp
+++ libtld/tld_emails.cpp
@@ -365,12 +365,9 @@ bool is_atom_char(char c)
  * returns false immediately and the count() function returns zero (0).
  */
 tld_email_list::tld_email_list()
-    //: f_input("") -- auto-init
     : f_flags(0)
     , f_result(TLD_RESULT_SUCCESS)
-    //, f_last_group("") -- auto-init
     , f_pos(0)
-    //, f_email_list() -- auto-init
 {
 }
 
