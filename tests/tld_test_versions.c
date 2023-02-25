796a8a9207c5e9ead997d87bed27b6424fdae6c9
AlexisWilke <alexis@m2osw.com>
2023-02-25 13:02:54 -0800

    Update copyrights to 2023.

--- tests/tld_test_versions.c
+++ tests/tld_test_versions.c
@@ -1,5 +1,5 @@
 /* TLD library -- test the TLD version in all files
- * Copyright (c) 2011-2022  Made to Order Software Corp.  All Rights Reserved
+ * Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
