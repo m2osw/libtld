b932e40a3a5f0601826454280b92d65ebc946dd3
AlexisWilke <alexis@m2osw.com>
2025-07-17 21:47:29 -0700

    Updated copyright notice to 2025.

--- libtld/tld_data.h
+++ libtld/tld_data.h
@@ -1,5 +1,5 @@
 /* TLD library -- TLD, domain name, and sub-domain extraction
- * Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
+ * Copyright (c) 2011-2025  Made to Order Software Corp.  All Rights Reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
--- libtld/tld_domain_to_lowercase.c
+++ libtld/tld_domain_to_lowercase.c
@@ -1,5 +1,5 @@
 /* TLD library -- encrypted domain name case folding
- * Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
+ * Copyright (c) 2011-2025  Made to Order Software Corp.  All Rights Reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
